e695a1d573dfc085a9c9145061353d48ad9f5c5e
jiyounglee-hr <2074titi@naver.com>
2025-04-18 17:07:40 +0900

    style: 도움말 내용 줄바꿈 수정

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ with st.sidebar:
         
         
     else:
-        st.markdown("<div class='upload-text'>Drag and drop file here<br>Limit 200MB per file • PDF</div>", unsafe_allow_html=True)
+        st.markdown("<div class='upload-text'>파일을 드래그&드롭해주세요.<br>200MB 이하의 PDF</div>", unsafe_allow_html=True)
 
     st.markdown("<br>", unsafe_allow_html=True)
     
@@ -378,9 +378,9 @@ with st.sidebar:
     st.markdown("<br>", unsafe_allow_html=True)
     with st.expander("도움말"):
         st.write("""
-       🤖 이력서분석 : PDF 형식의 이력서 파일을 업로드 > 채용요건 확인 > 경력기간 체크(필요 시) > '분석 시작하기' 
-       ☝️ 1차 면접 질문 : 직무기반의 경험, 프로젝트, 문제해결, 자격요건 관련 사례 질문
-       ✌️ 2차 면접 질문 : 핵심가치 기반의 [도전]두려워 말고 시도합니다, [책임감]대충은 없습니다, [협력]동료와 협업합니다, [전문성]능동적으로 일합니다
+        🤖 이력서분석 : PDF 형식의 이력서 파일을 업로드 > 채용요건 확인 > 경력기간 체크(필요 시) > '분석 시작하기' \n
+        ☝️ 1차 면접 질문 : 직무기반의 경험, 프로젝트, 문제해결, 자격요건 관련 사례 질문\n
+        ✌️ 2차 면접 질문 : 핵심가치 기반의 [도전]두려워 말고 시도합니다, [책임감]대충은 없습니다, [협력]동료와 협업합니다, [전문성]능동적으로 일합니다
         """)
 
 # 채용공고 데이터
